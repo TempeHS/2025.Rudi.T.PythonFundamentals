c97765152f86c15c612a0fcef15ba35bca6544e7
Rudi <116497284+yadead@users.noreply.github.com>
2024-02-15 22:41:30 +0000

    upload uncompleted deep.py

---
+++ 1-Conditionals/deep/deep.py
@@ -0,0 +1,11 @@
+def main():
+    x = str(
+        input("the answer to the Great Question of Life, the Universe and Everything")
+    )
+    if x == str(42) or x == str("forty two") or x == str("forty-two"):
+        print("Yes")
+    else:
+        print("No")
+
+
+main()
