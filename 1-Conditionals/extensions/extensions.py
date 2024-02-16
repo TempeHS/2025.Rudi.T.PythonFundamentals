94da406d7f714a685206f7984998d2e95ba058c3
Rudi <116497284+yadead@users.noreply.github.com>
2024-02-16 04:20:55 +0000

    Upload completed extensions.py

--- 1-Conditionals/extensions/extensions.py
+++ 1-Conditionals/extensions/extensions.py
@@ -1,10 +1,28 @@
 def main():
     x = str.lower(input("give me the name of a file"))
-    gif = x[-1:-5]
+    gif = x[-4:]
+    jpg = x[-4:]
+    jpeg = x[-5:]
+    png = x[-4:]
+    pdf = x[-4:]
+    txt = x[-4:]
+    zipz = x[-4:]
     if gif == str(".gif"):
         print("image/gif")
+    elif jpg == str(".jpg"):
+        print("image/jpeg")
+    elif jpeg == str(".jpeg"):
+        print("image/jpeg")
+    elif png == str(".png"):
+        print("image/png")
+    elif pdf == str(".pdf"):
+        print("application/pdf")
+    elif txt == str(".txt"):
+        print("text/plain")
+    elif zipz == str(".zip"):
+        print("application/zip")
     else:
-        print("fail")
+        print("application/octet-stream")
 
-
+    
 main()
