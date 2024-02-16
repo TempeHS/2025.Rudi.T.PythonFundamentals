d3d11842d25d9ba4f2cd4a8ca93b5f206e62a312
Rudi <116497284+yadead@users.noreply.github.com>
2024-02-16 00:53:07 +0000

    Uploaded uncompleted bank.py

---
+++ 1-Conditionals/bank/bank.py
@@ -0,0 +1,13 @@
+def main():
+    x = str.lower(input("give me a greeting"))
+    y = x[0]
+    z = x[0]
+    if z == str("hello"):
+        print("$0")
+    elif y == str("h"):
+        print("$20")
+    else:
+        print("$100")
+
+
+main()
