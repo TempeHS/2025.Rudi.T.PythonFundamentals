aa1276521a5ca1fb25f09f7174e505c2e707ccef
Rudi <116497284+yadead@users.noreply.github.com>
2024-02-22 23:34:42 +0000

    Upload uncompleted camel.py

--- 2-Loops/camel/camel.py
+++ 2-Loops/camel/camel.py
@@ -1,7 +1,18 @@
 def main():
     a = str(input("enter to get camel cased"))
-    b = a.split
 
+    for i in range(len(a)):
+        if a[i].isupper():
+            j = str(i)
+            print(a.replace("j", "_"))
 
 
 main()
+# def uppercase(input_str):
+# for i in range(len(input_str)):
+# if input_str[i].isupper():
+# return input_str[i]
+# return "no uppercase characters found"
+
+
+# print(uppercase(a))
--- classtasks/rockpaperscissors/rockpaperscissors.py
+++ classtasks/rockpaperscissors/rockpaperscissors.py
@@ -1,8 +1,38 @@
-def main():
-    playerchoice = str(input(rock paper scisors))
-    switch(playerchoice) {
-        case 
+import random
 
-    }
+
+def main():
+    playerchoice = str(input("rock paper scisors"))
+    choices = ["rock", "paper", "scissor"]
+    x = random.choice(choices)
+    match playerchoice:
+        case "rock":
+            if x == str("paper"):
+                print("you lose")
+            elif x == str("rock"):
+                print("you tie")
+            elif x == str("scissor"):
+                print("you win")
+            else:
+                print("nothing")
+        case "paper":
+            if x == str("paper"):
+                print("you tie")
+            elif x == str("rock"):
+                print("you win")
+            elif x == str("scissor"):
+                print("you lose")
+            else:
+                print("nothing")
+        case "scissor":
+            if x == str("paper"):
+                print("you win")
+            elif x == str("rock"):
+                print("you lose")
+            elif x == str("scissor"):
+                print("you tie")
+            else:
+                print("nothing")
+
 
 main()
